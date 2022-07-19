9df045940d7950fc5343bda42bcf4365319a2aeb
Mukunthan14 <108223678+Mukunthan14@users.noreply.github.com>
2022-07-19 18:18:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,4 @@ import streamlit as st
 import joblib
 st.title('LOGISTIC REGRESSION MODEL FOR STROKE')
 if st.button('Print accuracy score'):
-  model = joblib.load('Logistic regression model')
-  print(result)  
+  joblib.load('Logistic regression model')  
