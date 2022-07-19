9a41b34b13038b3fc9141bda7a2dca1978f14bf8
Mukunthan14 <108223678+Mukunthan14@users.noreply.github.com>
2022-07-19 19:11:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import joblib
+import pandas
+model = joblib.load('Logistic regression model')
 st.title('LOGISTIC REGRESSION MODEL FOR STROKE')
-if st.button('Print accuracy score'):
-  joblib.load('Logistic regression model')  
+age = st.number_input("age", min_value=1, max_value=100)
+hypertension = st.selectbox("hypertension",options=['yes', 'no'])
+heart_disease = st.selectbox("heart disease",options=['yes', 'no'])
+avg_glucose = st.number_input("average glucose level")
+bmi = st.number_input("bmi")
+sex = st.selectbox("Sex",options=['Male' , 'Female'])
+smoking = st.selectbox("smoking status",options=['formerly smoked', 'never smoked', 'smokes'])
