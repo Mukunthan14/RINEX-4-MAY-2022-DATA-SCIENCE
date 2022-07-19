a91fa360081f3a303928ae206a19c252f7e8e2ad
Mukunthan14 <108223678+Mukunthan14@users.noreply.github.com>
2022-07-19 17:37:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import joblib
 model = joblib.load('Logistic regression model')
 st.title('LOGISTIC REGRESSION MODEL FOR STROKE')
 if st.button('Print accuracy score'):
-  result = accuracy_score(y_pred,y_test)* 100
+  result = model.score(y_pred,y_test)*100
   print(result)  
